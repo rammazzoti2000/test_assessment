f5f96cc3d1b5da1ae7f2d20d02c189a21097a406
Alexandru <rammazzoti2000@gmail.com>
2021-03-23 04:28:00 +0100

    Fix dead links

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -11,8 +11,8 @@ const Pagination = ({ usersPerPage, totalUsers, paginate }) => {
     <nav>
       <ul className='pagination'>
         {pageNumbers.map(number => (
-          <li key={number} className='page-item'>
-            <a onClick={() => paginate(number)} href='!#' className='page-link'>
+          <li key={number} className='page-item'> {/* eslint-disable-next-line */}
+            <a onClick={() => paginate(number)} href='#' className='page-link'>
               {number}
             </a>
           </li>
